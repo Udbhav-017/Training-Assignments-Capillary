b5674dd5ef93f61721c4341cf4f0a22aa9db496e
Udbhav Gupta <rudbhav.gupta@gmail.com>
2023-02-22 12:45:15 +0530

    allwordsapproach issue: header size big, we need to store count map

--- src/main/java/com/huffmanzipper/wordbasedimplementation/compression/WordBasedCompressionImpl.java
+++ src/main/java/com/huffmanzipper/wordbasedimplementation/compression/WordBasedCompressionImpl.java
@@ -12,12 +12,22 @@ public class WordBasedCompressionImplDefault extends DefaultCompressorImpl {
     protected IMap<String, Integer> createFrequencyTable(IInputStream source) throws IOException{
         IMap<String, Integer> frequencyTable = new HashMapImpl<>();
 
-        int num;
+        char num;
         StringBuilder word = new StringBuilder();
 
-        while ((num = source.read()) != -1) {
+        while ((num = (char)source.read()) != -1) {
 
-            word.append((char) num);
+            if(Character.isLetter(num)){
+                word.append(num);
+            }
+            else{
+                frequencyTable.increment(word.toString(), 1);
+                frequencyTable.increment(String.valueOf(num), 1);
+                word = new 
+            }
+
+            if(word!="")
+                add(word)
 //            frequencyTable.increment(ch, 1);
         }
 
